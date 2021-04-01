95a097e4aec89c764ca7715acf4f046731b65a7d
xreider <xreider@yandex.ru>
2021-04-02 00:26:15 +1000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script>
+  import Player from './Player.svelte'
+  let redScore = 20
+  let blueScore = 20
+  $: blueWon = redScore <= 0
+  $: redWon = blueScore <= 0
+  $: gameOver = blueWon || redWon
+  function updateBlueScore(e) {
+    const updateScore = e.detail
+    blueScore += updateScore
+  }
+  function updateRedScore(e) {
+    const updateScore = e.detail
+    redScore += updateScore
+  }
+  function newGame() {
+    redScore = 20
+    blueScore = 20
+  }
+</script>
+
+<main>
+  <h1>Magic The Gathering Counter</h1>
+  <div id="controls-container">
+    <Player
+      {gameOver}
+      on:points={updateBlueScore}
+      fontColor="#0000AA"
+      won={blueWon}
+      winningText="Blue wins"
+      score={blueScore}
+    />
+    <Player
+      {gameOver}
+      on:points={updateRedScore}
+      fontColor="#AA0000"
+      won={redWon}
+      winningText="Red Wins"
+      score={redScore}
+    />
+  </div>
+  <button on:click={newGame}>Start Game</button>
+</main>
+
+<style>
+  main {
+    width: 80%;
+    padding: 20px;
+    border: solid gray 1px;
+    margin: 0 auto;
+    background-color: wheat;
+    margin: 10vh auto;
+  }
+  #controls-container {
+    display: flex;
+  }
+  button {
+    display: block;
+    width: 100%;
+    margin-top: 20px;
+    border: solid salmon 1px;
+    background-color: sandybrown;
+    color: rgb(61, 56, 56);
+    font-size: 20px;
+    border-radius: 3px;
+  }
+</style>
---
+++ src/Player.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  export let score
+  export let winningText
+  export let won
+  export let fontColor
+  export let gameOver = false
+  const dispatch = createEventDispatcher()
+  function minus() {
+    dispatch('points', -1)
+  }
+  function plus() {
+    dispatch('points', 1)
+  }
+</script>
+
+<div style="color: {fontColor}" class="player">
+  <h2>{score}</h2>
+  <button on:click={plus} disabled={gameOver} class="plus">+</button>
+  <button on:click={minus} disabled={gameOver} class="minus">-</button>
+  {#if won}
+    <h2>{winningText}</h2>
+  {/if}
+</div>
+
+<style>
+  .player {
+    flex-grow: 1;
+  }
+  .plus {
+    background-color: seagreen;
+  }
+  .minus {
+    background-color: brown;
+  }
+  button[disabled] {
+    background-color: gray;
+    pointer-events: none;
+  }
+  button {
+    font-size: 20px;
+    border-radius: 3px;
+    width: 40px;
+    color: white;
+    font-family: monospace;
+    font-weight: bold;
+  }
+</style>
